7cb6fd0f829c3b3fda3680112f6cdccbafc339f0
Tamilore Akinsola <tammy@Tamilores-MacBook-Air.local>
2025-06-17 14:58:49 +0100

    new change

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -8,13 +8,13 @@ function WorkCard() {
       <h3 className={styles.title}><i class="fa-solid fa-building"></i> Work</h3>
       {workData.map((job, index) => (
         <div key={index} className={styles.job}>
-          <div className={styles.left}>
+          <a href='https://thecodecenter.org'><div className={styles.left}>
             <img src={job.logo} alt={job.company} className={styles.logo} />
             <div className={styles.info}>
               <p className={styles.company}>{job.company}</p>
               <p className={styles.role}>{job.title}</p>
             </div>
-          </div>
+          </div></a>
           <div className={styles.right}>
             <p className={styles.duration}>{job.duration}</p>
           </div>
